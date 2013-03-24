6371cc60a39ec917ba66be25fbb42fba0b692a14
Julien Letessier <julien.letessier@gmail.com>
2013-03-24 18:40:23 +0000

    Normalizes strings passed fo #find

--- lib/blurrily/map.rb
+++ lib/blurrily/map.rb
@@ -5,15 +5,30 @@ module Blurrily
   Map.class_eval do
 
     def put_with_string_normalize(needle, reference, weight=0)
-      needle = needle.downcase
-      unless needle =~ /^([a-z ])+$/
-        needle = needle.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/,'').to_s.gsub(/[^a-z]/,' ')
-      end
-      needle.gsub!(/\s+/,' ')
-      needle.strip!
+      needle = normalize_string needle
       put_without_string_normalize(needle, reference, weight)
     end
 
     alias_method_chain :put, :string_normalize
+
+
+    def find_with_string_normalize(needle, limit=10)
+      needle = normalize_string needle
+      find_without_string_normalize(needle, limit)
+    end
+
+    alias_method_chain :find, :string_normalize
+
+
+    private
+
+    def normalize_string(needle)
+      result = needle.downcase
+      unless result =~ /^([a-z ])+$/
+        result = result.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/,'').to_s.gsub(/[^a-z]/,' ')
+      end
+      result.gsub(/\s+/,' ').strip
+    end
+
   end
 end
