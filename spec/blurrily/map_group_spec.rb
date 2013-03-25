6d2787fad81752d3f573f1662d1311d80fe2af84
Marcus Mitchell <marcusleemitchell@gmail.com>
2013-03-25 15:05:49 +0000

    [#46818783] select the correct map

--- spec/blurrily/map_group_spec.rb
+++ spec/blurrily/map_group_spec.rb
@@ -12,16 +12,27 @@ describe Blurrily::MapGroup do
       subject.new("test_dir").instance_variable_get('@dir').should == "test_dir"
     end
 
+    it "saves the current dir if no directory name is given" do
+      subject.new().instance_variable_get('@dir').should == "."
+    end
+
   end
 
   context "creating, loading and returning a db" do
     subject { described_class.new }
 
-    it "should return an instance of Map for a given DB" do
+    it "returns an instance of Map for a given DB" do
       subject.map("location_en").should be_a(Blurrily::Map)
     end
 
-    it "should create a Map if the given DB does not already exist"
+    it "returns the correct map, given the db name" do
+      map1 = subject.map('location_en')
+      map2 = subject.map('location_fr')
+      subject.map("location_en").object_id.should == map1.object_id
+      subject.map("location_en").object_id.should_not == map2.object_id
+    end
+
+    it "creates a Map if the given DB does not already exist"
 
   end
 
