d562bc7cf54fc4203a240c134eb3d3580c231431
Marcus Mitchell <marcusleemitchell@gmail.com>
2013-04-09 15:41:07 +0100

    bulk insert (PUT) data to blurrily

--- spec/blurrily/client_spec.rb
+++ spec/blurrily/client_spec.rb
@@ -66,4 +66,19 @@ describe Blurrily::Client do
       subject.put("London", 123, 0).should be_nil
     end
   end
+
+  context "bulk_put" do
+    it "fails if no needles are passed" do
+      expect { subject.bulk_put() }.to raise_error(ArgumentError)
+    end
+
+    it "fails if non array needles passed" do
+      expect { subject.bulk_put("needles") }.to raise_error(ArgumentError)
+    end
+
+    it "creates a well formed request command string" do
+      mock_tcp_next_request("OK", "PUT\tlocation_en\tLondon\t123\t0\nPUT\tlocation_en\tLondinium\t124\t0")
+      subject.bulk_put([["London",123,0],["Londinium",124,0]]).should be_nil
+    end
+  end
 end
