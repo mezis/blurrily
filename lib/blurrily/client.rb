de9f27a3f3fc060e45f1a948bcbc614066b80f9e
Julien Letessier <julien.letessier@gmail.com>
2014-09-29 16:50:32 +0200

    Cleans up yardoc

--- lib/blurrily/client.rb
+++ lib/blurrily/client.rb
@@ -8,42 +8,47 @@ module Blurrily
   class Client
     Error = Class.new(RuntimeError)
 
-    # Public: Initialize a new Blurrily::Client connection to Blurrily::Server.
+    # Initialize a new {Blurrily::Client} connection to {Blurrily::Server}.
     #
-    # host    - IP Address or FQDN of the Blurrily::Server.
+    # @param host IP Address or FQDN of the Blurrily::Server.
     #           Defaults to Blurrily::DEFAULT_HOST.
-    # port    - Port Blurrily::Server is listening on.
+    # @param port Port Blurrily::Server is listening on.
     #           Defaults to Blurrily::DEFAULT_PORT.
-    # db_name - Name of the data store being targeted.
+    # @param db_name Name of the data store being targeted.
     #           Defaults to Blurrily::DEFAULT_DATABASE.
     #
     # Examples
     #
-    #  Blurrily::Client.new('127.0.0.1', 12021, 'location_en')
-    #  # => #<Blurrily::Client:0x007fcd0d33e708 @host="127.0.0.1", @port=12021, @db_name="location_en">
+    # ```
+    # Blurrily::Client.new('127.0.0.1', 12021, 'location_en')
+    # # => #<Blurrily::Client:0x007fcd0d33e708 @host="127.0.0.1", @port=12021, @db_name="location_en">
+    # ```
     #
-    # Returns the instance of Blurrily::Client
+    # @returns the instance of {Blurrily::Client}
     def initialize(options = {})
       @host    = options.fetch(:host,     DEFAULT_HOST)
       @port    = options.fetch(:port,     DEFAULT_PORT)
       @db_name = options.fetch(:db_name,  DEFAULT_DATABASE)
     end
 
-    # Public: Find record references based on a given string (needle)
+    # Find record references based on a given string (needle)
     #
-    # needle - The string you're searching for matches on.
+    # @param needle The string you're searching for matches on.
     #          Must not contain tabs.
     #          Required
-    # limit  - Limit the number of results retruned (default: 10).
-    #          MUST be numeric.
+    # @param limit  Limit the number of results retruned (default: 10).
+    #          Must be numeric.
     #          Optional
     #
     # Examples
     #
-    #  @client.find('London')
-    #  # => [[123,6,3],[124,5,3]...]
+    # ```
+    # @client.find('London')
+    # # => [[123,6,3],[124,5,3]...]
+    # ```
     #
-    # Returns an Array of matching [REF,SCORE,WEIGHT] ordered by score. REF is the identifying value of the original record.
+    # @returns an Array of matching [`ref`,`score`,`weight`] ordered by score. `ref` is the identifying value of the original record.
+    # Note that unless modified, `weight` is simply the string length.
     def find(needle, limit = nil)
       limit ||= LIMIT_DEFAULT
       check_valid_needle(needle)
@@ -53,19 +58,21 @@ module Blurrily
       send_cmd_and_get_results(cmd).map(&:to_i).each_slice(3).to_a
     end
 
-    # Public: Index a given record.
+    # Index a given record.
     #
-    # db_name - The name of the data store being targeted. Required
-    # needle  - The string you wish to index. Must not contain tabs. Required
-    # ref     - The indentifying value of the record being indexed. Must be numeric. Required
-    # weight  - Weight of this particular reference. Default 0. Don't change unless you know what you're doing. Optional.
+    # @param db_name The name of the data store being targeted. Required
+    # @param needle The string you wish to index. Must not contain tabs. Required
+    # @param ref The indentifying value of the record being indexed. Must be numeric. Required
+    # @param weight Weight of this particular reference. Default 0. Don't change unless you know what you're doing. Optional.
     #
     # Examples
     #
-    #  @client.put('location_en', 'London', 123, 0)
-    #  # => OK
+    # ```
+    # @client.put('location_en', 'London', 123, 0)
+    # # => OK
+    # ```
     #
-    # Returns something to let you know that all is well.
+    # @returns something to let you know that all is well.
     def put(needle, ref, weight = 0)
       check_valid_needle(needle)
       check_valid_ref(ref)
